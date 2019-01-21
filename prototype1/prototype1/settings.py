21dd72b47877ff18e123bd2bb166dc1d9b5416f2
Ganesh Rajasekar <g3rajasekar@uwaterloo.ca>
2019-01-21 10:25:49 -0500

    Video Play Feature and buttons added by Ganesh
    Former-commit-id: c26e8819ff70d4903f27821c356a4f6c3e15c064

--- prototype1/prototype1/settings.py
+++ prototype1/prototype1/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'prototype1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'videos',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -119,3 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Add these new lines
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
