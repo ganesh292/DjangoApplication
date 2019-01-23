cec2e52aab8714e7b54f0e3d85612867a4a7e592
Navni Gupta <n57gupta@uwaterloo.ca>
2019-01-23 01:13:43 -0500

    updated views.py
    Former-commit-id: ee1ec677c0b9458b2d6eb408b6b239f1da6d3055

--- prototype1/feedback/views.py
+++ prototype1/feedback/views.py
@@ -3,12 +3,12 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 # Create your views here.
 def score(request):
-    return render(request,'feedback/sample.html')
+    return render(request,'sample.html')
 def score_submission(request):
     print("Hello score is uploaded")
     score= request.POST["score_input"]
     rating= score(score_input=score)
     rating.save()
-    return render(request,'feedback/sample.html')
+    return render(request,'sample.html')
 
 
