4100987eb5aefaa939aec1fe833252b553f75dc7
Navni Gupta <n57gupta@uwaterloo.ca>
2019-01-22 23:50:06 -0500

    score module
    Former-commit-id: 2365b97eb396888f5572f92b9f8643138760ff22

--- prototype1/feedback/models.py
+++ prototype1/feedback/models.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.db import models
+# Create your models here.
+class score(models.Model):
+    score_input = models.IntegerField()# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
 from django.db import models
 
--- prototype1/feedback/views.py
+++ prototype1/feedback/views.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.shortcuts import render
+# Create your views here.
+def score(request):
+    return render(request,'feedback/sample.html')
+def score_submission(request):
+    print("Hello score is uploaded")
+    score= request.POST["score_input"]
+    rating= score(score_input=score)
+    rating.save()
+    return render(request,'feedback/sample.html')# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
 from django.shortcuts import render
 
