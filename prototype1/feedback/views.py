1b3defeb2258441f79ab4c124b8f0de0d08e6201
Navni Gupta <n57gupta@uwaterloo.ca>
2019-01-24 01:08:40 -0500

    modified views.py
    Former-commit-id: 33abff54c2724c408539326e2cb8e41d79e674e3

--- prototype1/feedback/views.py
+++ prototype1/feedback/views.py
@@ -8,20 +8,20 @@ class score(TemplateView):
  	template_name = 'sample.html'
 	
 	def get(self,request):
-	form = ScoreForm()
-	return render(request,self.template_name, {'form':form})
+		form = ScoreForm()
+		return render(request,self.template_name, {'form':form})
 
 	def post(self,request):
-	form = ScoreForm(request.POST)
-	if form.is_valid():
-		post = form.save(commit=False)
-		post.user = request.user
-		post.save()
-		text = form.cleaned_data['post']
-		form = ScoreForm()
-		return redirect('sample')
-	args = {'form': form, 'text' : text}	
-	return render(request,self.template_name, args)
+		form = ScoreForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.user = request.user
+			post.save()
+			text = form.cleaned_data['post']
+			form = ScoreForm()
+			return redirect('sample')
+		args = {'form': form, 'text' : text}	
+		return render(request,self.template_name, args)
 
 
 
