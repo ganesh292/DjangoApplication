22232717e20465021ebecf5e70ec8f36031998e0
Ganesh Rajasekar <g3rajasekar@uwaterloo.ca>
2019-01-31 02:14:06 -0500

    Downloadfiles to local feature added
    Former-commit-id: 38ead6b7c717468f3c456797b739c7fd619fc50f

--- prototype1/videoplay/urls.py
+++ prototype1/videoplay/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from. import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', views.home,name='videoplay-home'),
     path('download/',views.download,name='videoplay-download'),
-    path('download/file',views.filedownload,name='videoplay-filedownload'),
     path('videos/',views.videos,name='videoplay-videos')
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- prototype1/videoplay/views.py
+++ prototype1/videoplay/views.py
@@ -1,44 +1,35 @@
 # from django.os import path
 import os
+from django.conf import settings
 from django.shortcuts import render
 from django.http    import HttpResponse
 from wsgiref.util import FileWrapper
-from django.templatetags.static import static
-try:
-    from StringIO import StringIO
-except ImportError:
-    from io import StringIO
-from zipfile import ZipFile
 
-# Create your views here.
+# urls = [
+#       "{% static 'video/video1.mp4' %}",
+#       "{% static 'video/video2.mp4' %}",
+#       "{% static 'video/video3.mp4' %}"',
 
+#       ]
+urls = [ 'video/video1.mp4', 'video/video2.mp4', 'video/video3.mp4',
 
-news =['video1','video2','video3']
+      ]
+stat_url = ["{% static url%}"]
 
-
+context1 = {'static':stat_url ,'urls': urls}
 
 # Create your views here.
 def home(request):
     return render(request,'videoplay/home.html')
 def videos(request):
     return render(request,'videoplay/play.html')
+
 def download(request):
-    return render(request,'videoplay/download.html')
 
-def filedownload(request):
+    return render(request,'videoplay/download.html',context1)
 
-    vid_list=[]
-    in_memory = StringIO()
-    zip = ZipFile(in_memory, "a")
-    zip.writestr('static/video/video1.mp4',"content")
 
-    response = HttpResponse(mimetype="application/zip")
-    response["Content-Disposition"] = "attachment; filename=two_files.zip"
-    
-    in_memory.seek(0)    
-    response.write(in_memory.read())
-    
-    return response
+
 
   
 
