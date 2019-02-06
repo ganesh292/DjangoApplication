69260ad31fc6fbe9ad80e995d0f76203eeb1dabe
Ganesh Rajasekar <g3rajasekar@uwaterloo.ca>
2019-02-06 02:02:13 -0500

    Changes imported from jatin
    Former-commit-id: 49588dc148012687b78847fa47318c2715132780

--- prototype1/videoplay/models.py
+++ prototype1/videoplay/models.py
@@ -9,3 +9,9 @@ class Video_Url(models.Model):
 
     def __str__(self):
         return self.vid_id
+    
+class Score_One_Stimulus(models.Model): 
+    session_id=models.IntegerField() 
+    user_name=models.CharField(max_length=10) 
+    vid_id=models.CharField(max_length=10) 
+    score=models.IntegerField()
