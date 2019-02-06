85ae6853b9006fb2139ec166e6aef6d8f50da97f
Ganesh Rajasekar <g3rajasekar@uwaterloo.ca>
2019-02-05 23:44:08 -0500

    Importing Ani's changes
    Former-commit-id: 345ddf725f18ad4640d9b1c8ad035f4d5768bf46

--- prototype1/videoplay/models.py
+++ prototype1/videoplay/models.py
@@ -3,3 +3,9 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Video_Url(models.Model):
+    vid_id = models.CharField(max_length=10,unique=True)
+    vid_url = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.vid_id
