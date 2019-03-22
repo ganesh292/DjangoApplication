27d244114d0c40203981190b2dd03a062ee3d931
jsapra <jsapra@uwaterloo.ca>
2019-03-22 12:00:30 -0400

    Jatin's new changes
    Former-commit-id: e9dd7247bf8243040ebb59a5f192e935c0b61f1d

--- prototype1/videoplay/tests.py
+++ prototype1/videoplay/tests.py
@@ -1,3 +1,5 @@
 from django.test import TestCase
-
+from videoplay.models import ScoreOneStimulus, VideoUrl,ScoreTwoStimulus
 # Create your tests here.
+# Class ScoreOneStimulusTest(TestCase):
+#     def 
