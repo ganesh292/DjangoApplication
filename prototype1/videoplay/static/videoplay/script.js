988889842310d4a08eb3a09143587b9797c85d79
Ganesh Rajasekar <g3rajasekar@uwaterloo.ca>
2019-03-22 11:53:44 -0400

    Revert "merging"
    This reverts commit 7c792995c674c849ed40e994461d9ffbb822e4a2 [formerly b443b8319af8326625f7d10f2a3631f9ac98ee0b]
    
    Former-commit-id: 008584d538504fc769c672809add8da1fd665c40

--- prototype1/videoplay/static/videoplay/script.js
+++ prototype1/videoplay/static/videoplay/script.js
@@ -8,8 +8,6 @@ var a=document.getElementById("scorelink");
 var vidId = document.getElementById("videoid");
 
 const param=new URLSearchParams(location.search);
-console.log(objs);
-
 var i=0;
 var score=0;
 var start=0;
@@ -87,36 +85,26 @@ var data = {
   'csrfmiddlewaretoken': csrftoken,
   'score': score
 }
-
-var data_preference = {
-  'csrfmiddlewaretoken': csrftoken,
-  'score': score_preference
-}
-
-
 var files={};
 //Selecting Videos
 function readFiles(event) {
   console.log("Inside ReadFile Function")
   files=document.getElementById("file").files;
   data['fileName']=files[i].name;
-  data_preference['video1']=files[i].name;
-  data_preference['video2']=files[i+1].name
-  loadAsUrl(files[i]);
   console.log(files.length)
   // console.log(data['fileName'])
   // console.log(files[i])
   // loadAsUrl(files[i]);
 }
 
-
+// localStorage.setItem('storeObj', JSON.stringify(myObj));
+// console.log(JSON.parse(localStorage.getItem('storeObj')));
 
 //Generating csrf token for POST operation
 
 //Data to POST
 
 //Function called after viewing all videos
-//Single Score
 function updateScore(){
   $.ajax({
       url: "/videoplay/videos2/",
@@ -133,30 +121,12 @@ function updateScore(){
     });
 }
 
-//Preference Score
-function updateScore_preference() {
-  $.ajax({
-    url: "/videoplay/temp/",
-    type: "POST",
-    //dataType: "json",
-    data: data_preference,
-    success: function (json) {
-      //console.log(files[i].name);
-      loadAsUrl(files[i]);
-    },
-    error: function (xhr, errmsg, err) {
-      alert("Could not send URL to Django. Error: " + xhr.status + ": " + xhr.responseText);
-    }
-  });
-}
-
-
 
 //Score slider
 slider.oninput = function() {
   output.innerHTML = this.value;
   score=this.value;
-  //data_preference['score'] = files[i].name;
+  data['score']=score;
 }
 
 
@@ -166,18 +136,6 @@ slider.oninput = function() {
 //   console.log(files[i]);
 //   console.log(i);
   
-// if (i == 3) {
-//   i=0;
-//   console.log(score);
-//   // updateScore();
-// }
-// data['score'] = JSON.stringify(data['score'])
-// updateScore_preference();
-// // loadAsUrl(files[i]);
-// slider.value = 50;
-// output.innerHTML = 50;
-// disableScroll();
-// }
 // if (i == 3) {
 //   i=0;
 //   console.log(score);
@@ -215,36 +173,6 @@ function disableScroll(){
   slider.style.opacity=0.2;
   slider.disabled=true;
 }
-//Series of events after video ends for single score
-// myVideo.addEventListener('ended', enableDisablebuttons,false);
-// function enableDisablebuttons(e) {
-//        slider.style.opacity=0.8;
-//        slider.disabled=false;
-//        slider.hidden=false;
-//        myVideo.style.display = "none";
-//        playButton.style.display="none";
-//        selVideo[0].style.display="none";
-//        submitButton.hidden=false;
-//        i++;
-//        document.getElementById("scoreDisp").hidden=false;
-//        document.exitFullscreen();
-// }
-
-function dummy(){
-  myVideo.play();
-  
-}
-//Series of events after video ends for preference score
-myVideo.addEventListener('ended', enableDisablebuttons, false);
-function enableDisablebuttons(e) {
-  i++;
-  if(i!=files.length){
-    loadAsUrl(files[i])
-    myVideo.autoplay=true;
-    window.setTimeout(dummy,5000)
-    
-  }
-  else{
 var vid_num = 1;
 //Series of events after video ends
 myVideo.addEventListener('ended', videoloop,false);
@@ -259,17 +187,10 @@ function enableDisablebuttons() {
        playButton.style.display="none";
        selVideo[0].style.display="none";
        submitButton.hidden=false;
+       i++;
        document.getElementById("scoreDisp").hidden=false;
        document.exitFullscreen();
-  }
 }
-}
-
-function sendScore(){
-  updateScore_preference();
-  window.location.href = "/videoplay/temp/"
-}
-
 //Loading the video files
 function loadAsUrl(theFile) {
     var reader = new FileReader();
@@ -280,27 +201,6 @@ function loadAsUrl(theFile) {
     reader.readAsDataURL(theFile);
 }
 //Play the videos
-function playVid(){
-// if(i==files.length){
-//     window.location.href="/videoplay/temp/"
-//   }
-
-    // if(i>0){
-    //   //data['score'] = JSON.stringify(data['score'])
-    //   //data_preference['video1'] = files[i-1].name;
-    //   //data_preference['video2'] = files[i].name;
-    //   //updateScore_preference();
-    //   submitButton.hidden = true;
-    //   slider.hidden = true;
-    //   slider.style.opacity = 0.2;
-    //   slider.disabled = true;
-    //   myVideo.autoplay=true;
-    // }
-
-    myVideo.style.display = "block";
-    playButton.style.display = "none";
-    myVideo.play();
-    console.log(i);
 // function playVid(){
 //     if(i>0){
 //       data['score'] = JSON.stringify(data['score'])
@@ -322,7 +222,7 @@ function playVid(){
 //     console.log(i);
     
 //   }
-}
+
 // //Change to full screen
 function toggleFullscreen() {
   console.log("Toggle Screen");
@@ -488,3 +388,4 @@ function videoloop(e)
 
 //Just to make sure static files are connected, see this message in console
 console.log("Hello! Static Cnnected");
+   
--- prototype1/videoplay/tests.py
+++ prototype1/videoplay/tests.py
@@ -1,33 +1,3 @@
-from django.test import TestCase, Client
-from django.urls import reverse,resolve
-from videoplay.views import home,download
-import json
+from django.test import TestCase
+
 # Create your tests here.
-
-class Tests(TestCase):
-
-#Testing URLs
-    def test_videoplay_home_urlisresolved(self):
-        url=reverse('videoplay-home')
-        self.assertEquals(resolve(url).func,home)
-
-    def test_videoplay_download_urlisresolved(self):
-        url = reverse('videoplay-download')
-        self.assertEquals(resolve(url).func, download)
-
-#Testing Views
-    def setUp(self):
-        self.client=Client()
-        self.home_url=reverse('videoplay-home')
-        self.download_url=reverse('videoplay-download')
-
-
-    def test_home_GET(self):
-        response = self.client.get(self.home_url)
-        self.assertEquals(response.status_code,200)
-        self.assertTemplateUsed(response,'videoplay/home.html')
-
-    def test_download_GET(self):
-        response = self.client.get(self.download_url)
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'videoplay/download.html')
